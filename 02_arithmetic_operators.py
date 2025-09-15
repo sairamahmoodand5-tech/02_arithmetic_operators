a99f345d0b3c4bb8393f67373a5aa37266944dda
sairamahmoodand5-tech <sairamahmoodand5@gmail.com>
2025-09-15 20:56:07 +0500

    Add files via upload

---
+++ 02_arithmetic_operators.py
@@ -0,0 +1,7 @@
+first_number = input ("Enter your first number:") 
+Second_number = input ("Enter your second number:") 
+operator = input ("Enter your operator:") # + - * /      
+
+result = first_number + operator + Second_number 
+Result = eval(result)
+print(Result)
